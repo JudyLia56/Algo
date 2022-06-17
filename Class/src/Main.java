b2b2ec51b35e65a12a604d8c823fa87dcb164180
JudyLia56 <judylia56@gmail.com>
2022-06-18 00:21:33 +0900

    .

--- Class/src/Main.java
+++ Class/src/Main.java
@@ -1,15 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-
-		for(int i = 0; i < n; i++) {
-			String str = br.readLine();
-			System.out.println(str.charAt(0)+""+str.charAt(str.length()-1));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] num = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		int find = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			if(num[i] == find) cnt++;
+		}
+		
+		System.out.println(cnt);
 	}
 }
